f8da3c9378383c317a39206dda8ca5364b3cbe45
Arthi-K <81531516+Arthi-K@users.noreply.github.com>
2022-11-27 17:43:52 -0800

    ADD Unit Tests

---
+++ src/app.py
@@ -0,0 +1,106 @@
+import re
+import os
+import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import Flask, render_template
+from flask import jsonify
+import logging
+from flask_caching import Cache
+template_dir = os.path.abspath('../templates/')
+config = {
+    "DEBUG": True,          
+    "CACHE_TYPE": "null",  
+    "CACHE_DEFAULT_TIMEOUT": 3600
+}
+
+app = Flask(__name__, template_folder=template_dir)
+app.config['SECRET_KEY'] = 'your secret key'
+app.config.from_mapping(config)
+cache = Cache(app)
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    app.logger.info("Connected Successfully to SQLite DB")
+    return conn
+
+def validate_url(url):
+    regex = re.compile(
+            r'^(?:http|ftp)s?://' # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+            r'localhost|' #localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+            r'(?::\d+)?' # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    app.logger.info("Checking if the URL is Malformed")
+    return re.match(regex, url) is not None
+
+@app.route('/v1/urlinfo/<path:url_addr>')
+@cache.cached(timeout=3600)
+def get_url(url_addr):
+    if url_addr[-1] == '/': url_addr = url_addr[:-1]
+    if not validate_url(url_addr):
+        return jsonify({
+            "message": "Bad Request Error, check the URL",
+            "safe": False
+        }), 400
+    conn = get_db_connection()
+    mal_url = conn.execute('SELECT * FROM url_table WHERE url_addr = ?',
+                     (url_addr,)).fetchone()
+    conn.close()
+    if mal_url is None:
+        return jsonify({
+            "message": "Access Allowed, NOT a Malware URL",
+            "safe": True
+        }), 200
+    return jsonify({
+            "message": "Error: Malware URL",
+            "safe": False
+        }), 403
+
+@app.route('/')
+@cache.cached(timeout=3600)
+def index():
+    conn = get_db_connection()
+    url_table = conn.execute('SELECT * FROM url_table').fetchall()
+    conn.close()
+    return render_template('index.html', url_table=url_table)
+
+@app.route('/delete/<path:url_addr>', methods = ('GET', ))
+def deleteUrl(url_addr):
+    conn = get_db_connection()
+    conn.execute('DELETE FROM url_table WHERE url_addr = ?',
+                     (url_addr,))
+    app.logger.info("Deleted successfully from the DB")
+    conn.commit()
+    conn.close()
+    
+    return jsonify({
+            "message": "Deleted",
+        }), 200
+
+@app.route('/create/', methods=('GET', 'POST'))
+@cache.cached(timeout=3600)
+def create():
+    if request.method == 'POST':
+        url_addr = request.form['title']
+        content = request.form['content']
+        # if url_addr[-1] == '/': url_addr = url_addr[:-1]
+        if not url_addr:
+            app.logger.error("Malware URL needs to be provided")
+            flash('Malware URL is required!')
+        elif not content:
+            app.logger.error("Content needs to be provided")
+            flash('Content is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO url_table (url_addr, content) VALUES (?, ?)',
+                         (url_addr, content))
+            app.logger.info("Inserted data successfully into the DB")
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('create.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ test/app_tests.py
@@ -0,0 +1,27 @@
+import pytest
+import requests
+
+def test_url_malware_200():
+    res = requests.get("http://127.0.0.1:5000/v1/urlinfo/https://www.w3schools.com/")
+    assert res.status_code == 200
+    assert res.json() == {"message": "Access Allowed, NOT a Malware URL",
+                            "safe": True}
+
+def test_url_malware_403():
+    res = requests.get("http://127.0.0.1:5000/v1/urlinfo/https://th-track-thailandpost.com/business/solutions/products/standard")
+    assert res.status_code == 403
+    assert res.json() == {"message": "Error: Malware URL",
+                            "safe": False}
+
+def test_malformed_url_400():
+    res = requests.get("http://127.0.0.1:5000/v1/urlinfo/https://www.caet-org.com/secure/citibank.com.th/online%20upd")
+    assert res.status_code == 400
+    assert res.json() == {"message": "Bad Request Error, check the URL",
+                            "safe": False}
+
+def test_delete_malware_url():
+    res = requests.get("http://127.0.0.1:5000/delete/https://www.caet-org.com/secure/citibank.com.th/online%20upd")
+    assert res.status_code == 200
+    assert res.json() == {"message": "Deleted"}
+
+
